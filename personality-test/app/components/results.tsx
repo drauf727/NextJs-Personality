68e3c79fe0f743f285d0603bc06884cb55aca71d
David Rauf <davidrauf@gmail.com>
2024-11-04 16:33:55 -0500

    test for Georgia

--- personality-test/app/components/results.tsx
+++ personality-test/app/components/results.tsx
@@ -4,6 +4,8 @@ interface ResultsProps {
     }
 }
 
+import Image from 'next/image'
+
 export default function Results({ answers }: ResultsProps) {
     // Calculate average score
     const averageScore = Object.values(answers).reduce((a, b) => a + b, 0) / Object.values(answers).length
@@ -13,9 +15,16 @@ export default function Results({ answers }: ResultsProps) {
             <h2 className="text-2xl font-bold text-center">Your Results</h2>
             <div className="text-center">
                 <p className="text-lg">Average Score: {averageScore.toFixed(1)}</p>
-                {/* Add more result analysis here */}
+                <p>Based on your average score it is safe to say you are an {averageScore > 3 ? "extrovert" : "introvert"} and would be an amazing wife for David. You should propose soon!!</p>
             </div>
-            {/* You can add detailed breakdowns or interpretations here */}
+            <div className="flex justify-center">
+                <Image 
+                    src="/photos/result.jpeg" 
+                    alt="Us" 
+                    width={200} 
+                    height={200} 
+                />
+            </div>
         </div>
     )
 }
